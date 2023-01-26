4a60511ed6b6da8be3e46e4c055f4e90dec9925d
Aymar <yannyao4066@gmail.com>
2023-01-26 00:22:06 +0000

    update commit

--- Exercice1/src/app/app.module.ts
+++ Exercice1/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { HomeComponent } from './home/home.component';
+import { MultiSlotComponent } from './multi-slot/multi-slot.component';
+import { ClosableWellComponent } from './closable-well/closable-well.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    HomeComponent,
+    MultiSlotComponent,
+    ClosableWellComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
